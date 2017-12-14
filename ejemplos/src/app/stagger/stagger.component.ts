e90319d3ca5f6ae9c11ad95c1c057a4595021994
Luis José Sánchez <luisjose@espiral.io>
2017-12-14 19:39:02 +0100

    Actualizando ejemplos.

--- ejemplos/src/app/stagger/stagger.component.ts
+++ ejemplos/src/app/stagger/stagger.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  stagger
+} from "@angular/animations";
 
 @Component({
   selector: 'app-stagger',
   templateUrl: './stagger.component.html',
-  styleUrls: ['./stagger.component.css']
+  styleUrls: ['./stagger.component.css'],
+  animations: [
+    trigger('animacionDeBotones', [
+
+      state('inactivo', style({ transform: 'translateX(-200px)'})),
+
+      transition('inactivo => activo', [
+        //query('button',style({ transform: 'translateX(-200px)'})),
+        query('button',
+          stagger('300ms', [
+            animate('400ms', style({ transform: 'translateX(200px)'}))
+        ]))
+      ]),
+
+      transition('activo => inactivo', [
+        query('button',
+          stagger('150ms', [
+            animate('200ms', style({ transform: 'translateX(-200px)'}))
+        ]))
+      ])
+    ])
+  ]
 })
 export class StaggerComponent implements OnInit {
+
+  estadoMenu = 'inactivo';
+
+  botones = ['Inicio', 'Contacto', 'Galería', 'Productos', 'Sobre nosotros'];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  activaMenu() {
+    this.estadoMenu = 'activo';
+    console.log(this.estadoMenu);
+  }
+
+  desactivaMenu() {
+    this.estadoMenu = 'inactivo';
+    console.log(this.estadoMenu);
+  }
 }
