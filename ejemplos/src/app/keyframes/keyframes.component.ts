13e9bb92e7294fe1df6b711df1364e5eadf5364a
Luis José Sánchez <luisjose@espiral.io>
2017-12-14 17:06:52 +0100

    Actualizando presentación.

--- ejemplos/src/app/keyframes/keyframes.component.ts
+++ ejemplos/src/app/keyframes/keyframes.component.ts
@@ -18,16 +18,15 @@ import {
       state("final", style({ opacity: 0 })),
       // Animación en varios pasos
       transition("inicial => final", animate("4000ms ease-in",
-          keyframes([
-            style({ transform: "translateX(200px) rotate(90deg)", offset: 0.2 }),
-            style({ transform: "translateX(-300px) rotate(180deg)", offset: 0.4 }),
-            style({ transform: "translateY(100px) rotate(270deg)", offset: 0.6 }),
-            style({ transform: "rotate(0deg)", offset: 0.8 }),
-            style({ transform: "rotate(90deg)", offset: 0.9 }),
-            style({ transform: "translateX(100px) scale(0.1) rotate(180deg)", offset: 1 })
-          ])
-        )
-      ),
+        keyframes([
+          style({ transform: "translateX(200px) rotate(90deg)", offset: 0.2 }),
+          style({ transform: "translateX(-300px) rotate(180deg)", offset: 0.4 }),
+          style({ transform: "translateY(100px) rotate(270deg)", offset: 0.6 }),
+          style({ transform: "rotate(0deg)", offset: 0.8 }),
+          style({ transform: "rotate(90deg)", offset: 0.9 }),
+          style({ transform: "translateX(100px) scale(0.1) rotate(180deg)", offset: 1 })
+        ])
+      )),
       transition("final => inicial", animate("3000ms"))
     ])
   ]
